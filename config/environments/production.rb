50c827efd977f0018523c87d86ae6d8d6dccd3c4
Higor Moreno Ribeiro <higormorenoribeiro@gmail.com>
2014-09-21 02:08:21 -0300

    Commit prod 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ config.i18n.fallbacks = true
     address:         ENV['MAILGUN_SMTP_SERVER'],
     user_name:       ENV['MAILGUN_SMTP_LOGIN'],
     password:        ENV['MAILGUN_SMTP_PASSWORD'],         
-    domain:          'desolate-hamlet-6224.herokuapp.com'
+    domain:          'desolate-hamlet-6224.herokuapp.com',
     authentication:  :plain
   }
 end
